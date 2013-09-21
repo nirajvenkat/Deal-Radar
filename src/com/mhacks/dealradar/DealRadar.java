4752eb8c2afcc27836fca6aecd7b432c018921f0
Sam Dickson <scdickso@purdue.edu>
2013-09-21 00:50:35 -0400

    Beginning action bar.

--- src/com/mhacks/dealradar/DealRadar.java
+++ src/com/mhacks/dealradar/DealRadar.java
@@ -1,23 +1,25 @@
 package com.mhacks.dealradar;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.ImageView;
 
 public class DealRadar extends Activity
 {
-
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(Bundle savedInstanceState)
+    {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.deal_radar, menu);
-		return true;
-	}
+        ActionBar actionBar = getActionBar();
+        actionBar.setCustomView(R.layout.action_bar);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setDisplayShowCustomEnabled(true);
+
+    }
 
 }
