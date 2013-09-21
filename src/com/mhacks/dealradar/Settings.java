eea3d09f07c67a376372c137ed59946fb8728b78
Niraj Venkat <venkatn@purdue.edu>
2013-09-21 14:15:59 -0400

    Drawer layout

---
+++ src/com/mhacks/dealradar/Settings.java
@@ -0,0 +1,18 @@
+package com.mhacks.dealradar;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by Niraj Venkat on 9/21/13.
+ */
+public class Settings extends FragmentActivity {
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_menu);
+    }
+}
