29a47b0a8f7cd0d86149c3feca2d781cba4f023d
Sam Dickson <scdickso@purdue.edu>
2013-09-21 01:14:06 -0400

    Testing getting BSSID and SSID

---
+++ src/com/mhacks/dealradar/Constants.java
@@ -0,0 +1,10 @@
+package com.mhacks.dealradar;
+
+/**
+ * Created by sdickson on 9/21/13.
+ */
+public class Constants
+{
+    public static final String PARSE_APPLICATION_ID = "XIyae4PpFDVrRGo0P7inT7j9WGxVUeNqHEMtwQGl";
+    public static final String PARSE_CLIENT_KEY = "sEIXfoVLOEh4ViH8FOUAzbCU3I682quauKFRQerR";
+}
--- src/com/mhacks/dealradar/DealRadar.java
+++ src/com/mhacks/dealradar/DealRadar.java
@@ -1,25 +1,63 @@
 package com.mhacks.dealradar;
 
 import android.app.ActionBar;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.app.Activity;
+import com.parse.Parse;
+import com.parse.ParseAnalytics;
+
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.List;
+
 public class DealRadar extends Activity
 {
+    WifiManager mainWifi;
+    WifiReceiver receiverWifi;
+    List<ScanResult> wifiList;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
     {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+        Parse.initialize(this,  Constants.PARSE_APPLICATION_ID, Constants.PARSE_CLIENT_KEY);
+        ParseAnalytics.trackAppOpened(getIntent());
         ActionBar actionBar = getActionBar();
         actionBar.setCustomView(R.layout.action_bar);
         actionBar.setDisplayShowHomeEnabled(false);
         actionBar.setDisplayShowCustomEnabled(true);
 
+        mainWifi = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        receiverWifi = new WifiReceiver();
+        registerReceiver(receiverWifi, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+        mainWifi.startScan();
+    }
+
+    public void onResume()
+    {
+        super.onResume();
+    }
+
+    class WifiReceiver extends BroadcastReceiver
+    {
+        public void onReceive(Context c, Intent intent)
+        {
+            wifiList = mainWifi.getScanResults();
+            for(int i = 0; i < wifiList.size(); i++)
+            {
+                Log.d("fatal", wifiList.get(i).SSID + ", " + wifiList.get(i).BSSID);
+            }
+        }
     }
 
 }
